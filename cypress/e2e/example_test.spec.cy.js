231f3e6795a2f6fe0798c6776bbabbaa0ec2361c
Lorena Santos de Oliveira <lorena.oliveira@muralis.com.br>
2024-10-03 14:59:36 -0300

    Atualizações de testes

---
+++ cypress/e2e/example_test.spec.cy.js
@@ -0,0 +1,8 @@
+// cypress/e2e/example_test.cy.js
+
+describe('Página inicial do Google', () => {
+  it('Deve carregar a página corretamente', () => {
+    cy.visit('https://www.google.com', { timeout: 10000 });  // Aumenta o timeout para carregar a página
+    cy.get('#APjFqb[name="q"]', { timeout: 10000 }).should('be.visible');  // Aumenta o timeout para encontrar o elemento
+  });
+});
